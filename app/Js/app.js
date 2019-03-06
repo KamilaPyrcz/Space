73516a662fdf647efeb04cd3110bf142bb09262f
Kamila <pyrcz.kamila@gmail.com>
2019-03-06 12:32:26 +0100

    contact page expansion (css)

--- app/Js/app.js
+++ app/Js/app.js
@@ -11,7 +11,7 @@ window.addEventListener('load', function () {
 
 //// Instantiate api handler
     const api = axios.create({
-        baseURL: 'http://localhost:3000/api',
+        baseURL: 'http://localhost:8000/api',
         timeout: 5000,
     });
 
--- gulpfile.js
+++ gulpfile.js
@@ -39,30 +39,25 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var prefix = require('gulp-autoprefixer');
-var browserSync = require('browser-sync').create();
-var reload = browserSync.reload;
 var nodemon = require('gulp-nodemon');
+var wait = require('gulp-wait');
+
+
+gulp.task('default', ['sass', 'nodemon'], function () {
+	gulp.watch('app/assets/stylesheets/**/_*.scss', ['sass'], );
+    gulp.watch('app/assets/stylesheets/*.scss', ['sass'], );
+	gulp.watch(['app/Js/*.js', 'app/*.html'],);
+});
+
 
 gulp.task('sass', function () {
 	return gulp.src('app/assets/stylesheets/**/*.scss')
+	.pipe(wait(200))
 	.pipe(sass({outputStyle: 'compressed', sourceComments: 'map'}, {errLogToConsole: true}))
 	.pipe(prefix("last 2 versions", "> 1%", "ie 8", "Android 2", "Firefox ESR"))
 	.pipe(gulp.dest('./app/assets/css'))
-	.pipe(reload({stream:true}));
 });
 
-gulp.task('browser-sync', ['nodemon'], function() {
-	browserSync.init(null, {
-		proxy: "localhost:8000"
-		
-	});
-});
-
-gulp.task('default', ['sass', 'browser-sync'], function () {
-	gulp.watch('app/assets/stylesheets/**/_*.scss', ['sass'], reload);
-    gulp.watch('app/assets/stylesheets/*.scss', ['sass'], reload);
-	gulp.watch(['app/Js/*.js', 'app/*.html'], reload);
-});
 
 gulp.task('nodemon', function (cb) {
 	var called = false;
